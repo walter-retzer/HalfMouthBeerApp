74ea9260de21db84cc48a7f233e423c68f9116fd
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-13 18:36:01 -0300

    Ajuste de versão de bibliotecas.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,19 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.application").version("8.0.1").apply(false)
+    id("com.android.library").version("8.0.1").apply(false)
+    id("org.jetbrains.compose").version("1.4.0").apply(false)
+    kotlin("android").version("1.8.10").apply(false)
+    kotlin("multiplatform").version("1.8.10").apply(false)
+}
+
 buildscript {
     dependencies {
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.5")
+        classpath("dev.icerock.moko:resources-generator:0.22.3")
     }
 }
 
-plugins {
-    //trick: for the same plugin versions in all sub-modules
-    kotlin("multiplatform").apply(false)
-    id("com.android.application").apply(false)
-    id("com.android.library").apply(false)
-    id("org.jetbrains.compose").apply(false)
-}
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
