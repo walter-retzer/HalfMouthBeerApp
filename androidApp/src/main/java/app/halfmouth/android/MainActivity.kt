6a93ce0a6dc7b4ffeaf0cdb2d6733b7c89fd394f
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-13 21:11:03 -0300

    Ajuste da exibição da imagem e texto da string do resource compartilhado.

--- androidApp/src/main/java/app/halfmouth/android/MainActivity.kt
+++ androidApp/src/main/java/app/halfmouth/android/MainActivity.kt
@@ -3,19 +3,66 @@ package app.halfmouth.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import app.halfmouth.App
+import app.halfmouth.Strings
+import dev.icerock.moko.resources.StringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App(
-                darkTheme = isSystemInDarkTheme(),
-                dynamicColor = true
-            )
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = app.halfmouth.android.R.drawable.halfmouth),
+                            contentDescription = null
+                        )
+                        Text(
+                            text = stringResource(
+                                id = SharedRes.strings.HalfMouth
+                            )
+                        )
+                    }
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun stringResource(id: StringResource, vararg args: Any): String {
+    return Strings(LocalContext.current).get(id, args.toList())
+}
+
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        GreetingView("HalfMouth")
     }
 }
