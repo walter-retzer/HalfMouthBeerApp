d72086c01dbb4479fb1bc8b3f9501b30c085bf86
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-14 13:16:14 -0300

    Ajuste do compileOptions do gradle

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -10,7 +10,7 @@ kotlin {
     android {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "17"
+                jvmTarget = "1.8"
             }
         }
     }
@@ -93,8 +93,8 @@ android {
         minSdk = 24
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
