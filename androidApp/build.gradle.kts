25e6bfc818819a78efa5e5d69c8a822cb6c28ce5
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-14 13:15:31 -0300

    Adicionado lib do compose android matrial3

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -45,7 +45,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:1.5.4")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
     implementation("androidx.compose.foundation:foundation:1.5.4")
-    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.material:material:1.4.0")
     implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.material3:material3:1.1.2")
     debugImplementation(libs.compose.ui.tooling)
 }
