d529d11f3c4f685bc5b050e0e7b9651d81454864
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-23 11:29:49 -0300

    Inclusão da rota da tela da Splash Screen

--- androidApp/src/main/java/app/halfmouth/android/navigation/Navigation.kt
+++ androidApp/src/main/java/app/halfmouth/android/navigation/Navigation.kt
@@ -7,11 +7,16 @@ import androidx.navigation.compose.rememberNavController
 import app.halfmouth.android.screen.ChartScreen
 import app.halfmouth.android.screen.MainScreen
 import app.halfmouth.android.screen.ScreenRoute
+import app.halfmouth.android.screen.SplashScreen
+
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = ScreenRoute.MainScreen.route) {
+    NavHost(navController = navController, startDestination = ScreenRoute.SplashScreen.route) {
+        composable(route = ScreenRoute.SplashScreen.route) {
+            SplashScreen(navController)
+        }
         composable(route = ScreenRoute.MainScreen.route) {
             MainScreen(navController)
         }
--- androidApp/src/main/java/app/halfmouth/android/screen/ScreenRoute.kt
+++ androidApp/src/main/java/app/halfmouth/android/screen/ScreenRoute.kt
@@ -1,6 +1,7 @@
 package app.halfmouth.android.screen
 
 sealed class ScreenRoute(val route: String){
+    object SplashScreen : ScreenRoute("splash_screen")
     object MainScreen : ScreenRoute("main_screen")
     object ChartScreen : ScreenRoute("chart_screen")
 }
