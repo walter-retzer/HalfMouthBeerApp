0949ecffaf1dd722aa348d19b08e142e161b257c
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-23 15:55:54 -0300

    Inclusão de parametros na class ScreenRoute

--- androidApp/src/main/java/app/halfmouth/android/screen/ScreenRoute.kt
+++ androidApp/src/main/java/app/halfmouth/android/screen/ScreenRoute.kt
@@ -1,7 +1,16 @@
 package app.halfmouth.android.screen
 
-sealed class ScreenRoute(val route: String){
-    object SplashScreen : ScreenRoute("splash_screen")
-    object MainScreen : ScreenRoute("main_screen")
-    object ChartScreen : ScreenRoute("chart_screen")
+import app.halfmouth.android.R
+
+sealed class ScreenRoute(val route: String, val title: String, val icon: Int) {
+    object SplashScreen : ScreenRoute("splash_screen", "Splash", R.drawable.icon_water)
+    object MainScreen : ScreenRoute("main_screen", "Menu", R.drawable.icon_water)
+    object ChartScreen : ScreenRoute("chart_screen", "Gráficos", R.drawable.icon_water)
 }
+
+fun listOfBottomBarScreen(): List<ScreenRoute> = listOf(
+    ScreenRoute.MainScreen,
+    ScreenRoute.ChartScreen,
+    ScreenRoute.MainScreen,
+    ScreenRoute.ChartScreen,
+)
