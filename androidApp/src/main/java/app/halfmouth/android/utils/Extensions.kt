4c88025f9625baf89073a8e11ee5de37e233e597
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-24 18:36:56 -0300

    Adicionado try catch na fun formattedDate().

--- androidApp/src/main/java/app/halfmouth/android/utils/Extensions.kt
+++ androidApp/src/main/java/app/halfmouth/android/utils/Extensions.kt
@@ -7,9 +7,18 @@ import java.util.TimeZone
 
 
 fun String.formattedDate(): String {
-    val dateFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale("pt", "BR"))
-    dateFormat.timeZone = TimeZone.getTimeZone("UTC")
-    val date = dateFormat.parse(this)
-    val dateTimeFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale("pt", "BR"))
-    return dateTimeFormat.format(date)
+    var string = ""
+    try {
+        val dateFormat: DateFormat = SimpleDateFormat(
+            "yyyy-MM-dd'T'HH:mm:ss'Z'",
+            Locale("pt", "BR")
+        )
+        dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+        val date = dateFormat.parse(this)
+        val dateTimeFormat = SimpleDateFormat("dd/MM/yyyy - HH:mm:ss", Locale("pt", "BR"))
+        string = dateTimeFormat.format(date)
+    } catch (e: Exception) {
+        println("Erro ao Converter String: $e")
+    }
+    return string
 }
