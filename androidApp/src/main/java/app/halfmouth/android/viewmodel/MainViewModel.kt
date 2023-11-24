959865a8fa52c8c330f85b9ffd8144fe67b32a6f
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-24 03:24:14 -0300

    Inclusão da MainViewModel

---
+++ androidApp/src/main/java/app/halfmouth/android/viewmodel/MainViewModel.kt
@@ -0,0 +1,38 @@
+package app.halfmouth.android.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.halfmouth.android.data.api.ApiService
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val service = ApiService.create()
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        loadStuff()
+    }
+
+    fun request() {
+        loadStuff()
+        viewModelScope.launch {
+            service.getThingSpeakValues("2")
+        }
+    }
+
+
+
+    fun loadStuff() {
+        viewModelScope.launch {
+            service.getThingSpeakValues("2")
+            _isLoading.value = true
+            delay(3000L)
+            _isLoading.value = false
+        }
+    }
+}
