f2de5552a49a4ccc26b05eca422ba6dc5b6ee6ef
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-27 19:10:48 -0300

    Adicionado animação de transição de tela para o ProductionScreen.

--- androidApp/src/main/java/app/halfmouth/android/navigation/Navigation.kt
+++ androidApp/src/main/java/app/halfmouth/android/navigation/Navigation.kt
@@ -3,7 +3,10 @@ package app.halfmouth.android.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import app.halfmouth.android.navigation.NavAnimations.slideDownExitAnimation
+import app.halfmouth.android.navigation.NavAnimations.slideUpEnterAnimation
 import app.halfmouth.android.screen.ChartScreen
 import app.halfmouth.android.screen.HomeScreen
 import app.halfmouth.android.screen.ProductionScreen
@@ -21,11 +24,22 @@ fun Navigation() {
         composable(route = ScreenRoute.HomeScreen.route) {
             HomeScreen(navController)
         }
-        composable(route = ScreenRoute.ProductionScreen.route) {
-            ProductionScreen(navController)
-        }
-        composable(route = ScreenRoute.ChartScreen.route) {
-            ChartScreen(navController)
+        navigation(
+            startDestination = ScreenRoute.ProductionScreen.route,
+            route = "navigation_production"
+        ) {
+
+            composable(
+                route = ScreenRoute.ProductionScreen.route,
+                enterTransition = slideUpEnterAnimation,
+                exitTransition = slideDownExitAnimation
+
+            ) {
+                ProductionScreen(navController)
+            }
+            composable(route = ScreenRoute.ChartScreen.route) {
+                ChartScreen(navController)
+            }
         }
     }
 }
