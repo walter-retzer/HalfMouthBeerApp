5962a633952427c9d5120de5d47aa21a305d4123
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-28 16:41:18 -0300

    Adicionado class GoogleAuthUiClient com as funções de signIn e singOut utilizando uma conta google

---
+++ androidApp/src/main/java/app/halfmouth/android/data/googleAuth/GoogleAuthUiClient.kt
@@ -0,0 +1,89 @@
+package app.halfmouth.android.data.googleAuth
+
+import android.content.Intent
+import android.content.IntentSender
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.BeginSignInRequest.GoogleIdTokenRequestOptions
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.Firebase
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.tasks.await
+import java.util.concurrent.CancellationException
+
+class GoogleAuthUiClient(private val oneTapClient: SignInClient) {
+
+    private val auth = Firebase.auth
+
+    suspend fun signIn(): IntentSender? {
+        val result = try {
+            oneTapClient.beginSignIn(
+                buildSignInRequest()
+            ).await()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+            null
+        }
+        return result?.pendingIntent?.intentSender
+    }
+
+    suspend fun signInWithIntent(intent: Intent): SignInResult {
+        val credentials = oneTapClient.getSignInCredentialFromIntent(intent)
+        val googleIdToken = credentials.googleIdToken
+        val googleCredentials = GoogleAuthProvider.getCredential(googleIdToken, null)
+
+        return try {
+            val user = auth.signInWithCredential(googleCredentials).await().user
+            SignInResult(
+                data = user?.run {
+                    UserId(
+                        userId = uid,
+                        userName = displayName.toString(),
+                        profilePictureUrl = photoUrl.toString()
+
+                    )
+                },
+                errorMessage = null
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+            SignInResult(
+                data = null,
+                errorMessage = e.message.toString()
+            )
+        }
+    }
+
+    private fun buildSignInRequest(): BeginSignInRequest {
+        return BeginSignInRequest.Builder()
+            .setGoogleIdTokenRequestOptions(
+                GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId("688786125580-mi38hno1leh9cdcpqifa2ema9pkkb1et.apps.googleusercontent.com")
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
+    }
+
+    suspend fun signOut() {
+        try {
+            oneTapClient.signOut().await()
+            auth.signOut()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+        }
+    }
+
+    fun getSignedInUser(): UserId? = auth.currentUser?.run {
+        UserId(
+            userId = uid,
+            userName = displayName.toString(),
+            profilePictureUrl = photoUrl.toString()
+        )
+    }
+}
