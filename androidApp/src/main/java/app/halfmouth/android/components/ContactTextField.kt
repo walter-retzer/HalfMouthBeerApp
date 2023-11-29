adcb35912e9ddc19f7cb5ec1d5fafc5b3c7e5e55
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-29 19:28:58 -0300

    Ajuste da fun ContactTextField receber o erro e type do input

--- androidApp/src/main/java/app/halfmouth/android/components/ContactTextField.kt
+++ androidApp/src/main/java/app/halfmouth/android/components/ContactTextField.kt
@@ -3,11 +3,13 @@ package app.halfmouth.android.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -16,7 +18,9 @@ fun ContactTextField(
     placeholder: String,
     error: String?,
     onValueChanged: (String) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    inputType: KeyboardOptions,
+    errorReset: () -> Unit
 ) {
     Column(modifier) {
         OutlinedTextField(
@@ -24,9 +28,12 @@ fun ContactTextField(
             placeholder = { Text(text = placeholder) },
             onValueChange = onValueChanged,
             shape = RoundedCornerShape(20.dp),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged { errorReset() },
+            keyboardOptions = inputType,
         )
-        if(error != null) {
+        if (error != null) {
             Text(
                 text = error,
                 color = MaterialTheme.colorScheme.error
