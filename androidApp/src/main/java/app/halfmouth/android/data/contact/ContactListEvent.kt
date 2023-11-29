f3ac00f1926467add0fef91873ec2c39daf8cc81
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-29 10:09:43 -0300

    Criado interface para os campos de inputTextField de cadastro do usuário

---
+++ androidApp/src/main/java/app/halfmouth/android/data/contact/ContactListEvent.kt
@@ -0,0 +1,8 @@
+package app.halfmouth.android.data.contact
+
+sealed interface ContactListEvent {
+    data class OnFirstNameChanged(val value: String): ContactListEvent
+    data class OnLastNameChanged(val value: String): ContactListEvent
+    data class OnEmailChanged(val value: String): ContactListEvent
+    data class OnPhoneNumberChanged(val value: String): ContactListEvent
+}
