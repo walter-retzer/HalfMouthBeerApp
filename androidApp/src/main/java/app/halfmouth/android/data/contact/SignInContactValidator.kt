95736057f99ec41638e73ef4c12da907cb09e641
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-29 23:56:45 -0300

    Inserido validadores dos inputTextField das senhas.

--- androidApp/src/main/java/app/halfmouth/android/data/contact/SignInContactValidator.kt
+++ androidApp/src/main/java/app/halfmouth/android/data/contact/SignInContactValidator.kt
@@ -6,20 +6,36 @@ object SignInContactValidator {
         var result = ValidationResult()
 
         if(contact.firstName.isBlank()) {
-            result = result.copy(firstNameError = "The first name can't be empty.")
+            result = result.copy(firstNameError = "O nome não foi preenchido.")
         }
 
-        if(contact.lastName.isBlank()) {
-            result = result.copy(lastNameError = "The last name can't be empty.")
+        if(contact.phoneNumber.isBlank()) {
+            result = result.copy(phoneNumberError = "O celular não foi preenchido.")
         }
 
         val emailRegex = Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
         if(!emailRegex.matches(contact.email)) {
-            result = result.copy(emailError = "This is not a valid email.")
+            result = result.copy(emailError = "Digite um email válido")
         }
 
-        if(contact.phoneNumber.isBlank()) {
-            result = result.copy(phoneNumberError = "The phone number can't be empty.")
+        if(contact.password.isBlank()) {
+            result = result.copy(passwordError = "A senha esta vazia.")
+        }
+
+        if(contact.confirmPassword.isBlank()) {
+            result = result.copy(passwordError = "A confirma senha esta vazia.")
+        }
+
+        if(contact.password.length <= 5) {
+            result = result.copy(passwordError = "A senha deve conter 6 dígitos.")
+        }
+
+        if(contact.confirmPassword.length <= 5) {
+            result = result.copy(confirmPasswordError = "A confirma senha deve conter 6 dígitos.")
+        }
+
+        if(contact.password != contact.confirmPassword) {
+            result = result.copy(confirmPasswordError = "Verifique as senhas digitadas.")
         }
 
         return result
@@ -27,8 +43,9 @@ object SignInContactValidator {
 
     data class ValidationResult(
         val firstNameError: String? = null,
-        val lastNameError: String? = null,
-        val emailError: String? = null,
         val phoneNumberError: String? = null,
+        val emailError: String? = null,
+        val passwordError: String? = null,
+        val confirmPasswordError: String? = null,
     )
 }
