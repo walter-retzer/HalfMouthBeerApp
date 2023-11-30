6fe3ce2097eb8c038dbac286aeb7eda522f5b37a
WDRETZER <walter_retzer@yahoo.com.br>
2023-11-29 23:49:00 -0300

    Inserido campo icon para exibir/esconder senhas.

--- androidApp/src/main/java/app/halfmouth/android/components/ContactTextField.kt
+++ androidApp/src/main/java/app/halfmouth/android/components/ContactTextField.kt
@@ -1,15 +1,26 @@
 package app.halfmouth.android.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -20,8 +31,11 @@ fun ContactTextField(
     onValueChanged: (String) -> Unit,
     modifier: Modifier = Modifier,
     inputType: KeyboardOptions,
-    errorReset: () -> Unit
+    errorReset: () -> Unit,
+    trailingIcon: @Composable() (() -> Unit)?
 ) {
+    var passwordVisibility: Boolean by remember { mutableStateOf(false) }
+
     Column(modifier) {
         OutlinedTextField(
             value = value,
@@ -32,6 +46,16 @@ fun ContactTextField(
                 .fillMaxWidth()
                 .onFocusChanged { errorReset() },
             keyboardOptions = inputType,
+            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                    Icon(
+                        modifier = Modifier.clickable { passwordVisibility = !passwordVisibility },
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = null,
+                    )
+                }
+            }
         )
         if (error != null) {
             Text(
