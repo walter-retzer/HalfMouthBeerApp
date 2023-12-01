f5cc6edcc306effc7740e5caf7ad76ba3d9e2728
WDRETZER <walter_retzer@yahoo.com.br>
2023-12-01 14:03:53 -0300

    Incluido class para as Constants do App.

---
+++ androidApp/src/main/java/app/halfmouth/android/utils/Constants.kt
@@ -0,0 +1,12 @@
+package app.halfmouth.android.utils
+
+class Constants {
+    companion object{
+        const val USER_UID = "user_uid"
+        const val USER_NAME = "user_name"
+        const val USER_IMAGE = "user_image"
+        const val USER_EMAIL = "user_email"
+        const val USER_CELLPHONE = "user_cellphone"
+        const val PHONE_MAX_NUMBER = 11
+    }
+}
