0827f6dfd11bd6c1cdbebcb6e567ca2570c83365
WDRETZER <walter_retzer@yahoo.com.br>
2023-12-01 17:13:45 -0300

    Ajuste de imagem para o perfil default para user google

--- androidApp/src/main/java/app/halfmouth/android/screen/Profile.kt
+++ androidApp/src/main/java/app/halfmouth/android/screen/Profile.kt
@@ -1,16 +1,18 @@
 package app.halfmouth.android.screen
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,9 +30,11 @@ import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
 import app.halfmouth.android.R
+import app.halfmouth.android.components.BottomBarMenu
 import app.halfmouth.android.data.googleAuth.GoogleAuthUiClient
 import app.halfmouth.android.security.SecurePreferencesApp
 import app.halfmouth.android.utils.Constants
+import app.halfmouth.theme.SurfaceVariantDark
 import app.halfmouth.theme.YellowContainerLight
 import coil.compose.rememberAsyncImagePainter
 import com.google.android.gms.auth.api.identity.Identity
@@ -39,93 +43,135 @@ import com.google.android.gms.auth.api.identity.Identity
 fun ProfileScreen(navController: NavHostController) {
     val context = LocalContext.current
     val pref = SecurePreferencesApp()
-    val userImage = pref.get(Constants.USER_IMAGE) ?: ""
-    val userName = pref.get(Constants.USER_NAME) ?: ""
-
     val googleAuthUiClient by lazy {
         GoogleAuthUiClient(oneTapClient = Identity.getSignInClient(context))
     }
 
-    ConstraintLayout(
-        modifier = Modifier
-            .background(Color.Black)
-            .fillMaxSize()
-    ) {
-        val (text, rectangle, image) = createRefs()
 
-        Text(
-            text = "Home",
-            style = TextStyle(
-                color = YellowContainerLight,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.SansSerif
-            ),
-            modifier = Modifier
-                .constrainAs(text) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-        )
+    BackHandler { }
 
-
-
-
-
-        Canvas(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 80.dp)
-                .constrainAs(rectangle) {
-                    top.linkTo(text.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-        ) {
-            val size = size
-            drawRoundRect(
-                Color.White,
-                topLeft = Offset(0f, -100f),
-                size = size,
-                cornerRadius = CornerRadius(30.dp.toPx(), 30.dp.toPx()),
-            )
+    Scaffold(
+        scaffoldState = rememberScaffoldState(),
+        bottomBar = {
+            BottomBarMenu(navController = navController)
         }
+    ) {
+        Box(modifier = Modifier.padding(it)) {
+            val init = googleAuthUiClient.getSignedInUser() != null
 
-        if (userImage.isNotEmpty()) {
-            Image(
-                painter = rememberAsyncImagePainter(userImage),
-                contentDescription = userName,
-                contentScale = ContentScale.Crop,
+            val userGoogle = pref.get(Constants.USER_GOOGLE_SIGNIN) ?: false
+            val userSignInDefault = pref.get(Constants.USER_DEFAULT_SIGNIN) ?: false
+            val userImage = pref.get(Constants.USER_IMAGE) ?: ""
+            val userName = pref.get(Constants.USER_NAME) ?: ""
+            val userEmail = pref.get(Constants.USER_EMAIL) ?: ""
+
+            ConstraintLayout(
                 modifier = Modifier
-                    .size(100.dp)
-                    .offset(25.dp, 70.dp)
-                    .clip(RoundedCornerShape(35))
-                    .constrainAs(image) {
-                        top.linkTo(rectangle.top)
-                        start.linkTo(parent.start)
-                    }
-            )
-        } else {
-            Image(
-                painter = painterResource(id = R.drawable.profile_default_image),
-                contentDescription = userName,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(100.dp)
-                    .offset(25.dp, 70.dp)
-                    .clip(RoundedCornerShape(35))
-                    .constrainAs(image) {
-                        top.linkTo(rectangle.top)
-                        start.linkTo(parent.start)
-                    }
-            )
+                    .background(Color.Black)
+                    .fillMaxSize()
+            ) {
+                val (text, rectangle, image, name, email) = createRefs()
+
+                Text(
+                    text = "Perfil",
+                    style = TextStyle(
+                        color = YellowContainerLight,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif
+                    ),
+                    modifier = Modifier
+                        .padding(top = 25.dp)
+                        .constrainAs(text) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }
+                )
+
+                Canvas(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 80.dp)
+                        .constrainAs(rectangle) {
+                            top.linkTo(text.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }
+                ) {
+                    val size = size
+                    drawRoundRect(
+                        SurfaceVariantDark,
+                        topLeft = Offset(0f, -100f),
+                        size = size,
+                        cornerRadius = CornerRadius(30.dp.toPx(), 30.dp.toPx()),
+                    )
+                }
+
+                if (userGoogle && init) {
+                    val imageUrl = userImage.ifEmpty { Constants.USER_DEFAULT_IMAGE }
+                    Image(
+                        painter = rememberAsyncImagePainter(imageUrl),
+                        contentDescription = userName,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(100.dp)
+                            .offset(25.dp, 70.dp)
+                            .clip(RoundedCornerShape(35))
+                            .constrainAs(image) {
+                                top.linkTo(rectangle.top)
+                                start.linkTo(parent.start)
+                            }
+                    )
+                }
+                if(userSignInDefault) {
+                    Image(
+                        painter = painterResource(id = R.drawable.profile_default_image),
+                        contentDescription = userName,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(100.dp)
+                            .offset(25.dp, 70.dp)
+                            .clip(RoundedCornerShape(35))
+                            .constrainAs(image) {
+                                top.linkTo(rectangle.top)
+                                start.linkTo(parent.start)
+                            },
+                    )
+                }
+
+                Text(
+                    text = userName,
+                    style = TextStyle(
+                        color = YellowContainerLight,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif
+                    ),
+                    modifier = Modifier
+                        .padding(top = 25.dp)
+                        .constrainAs(name) {
+                            top.linkTo(image.bottom)
+                            start.linkTo(parent.start)
+                        }
+                )
+
+                Text(
+                    text = userEmail,
+                    style = TextStyle(
+                        color = YellowContainerLight,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif
+                    ),
+                    modifier = Modifier
+                        .padding(top = 25.dp)
+                        .constrainAs(email) {
+                            top.linkTo(name.bottom)
+                            start.linkTo(parent.start)
+                        }
+                )
+            }
         }
     }
-}
-
-@Composable
-fun UserContactPhoto() {
-
-
 }
