d38cc168d37cc4c1ddd4487bf6ae8b1f909d4fb7
WDRETZER <walter_retzer@yahoo.com.br>
2023-12-05 11:21:30 -0300

    Ajuste da tela para o nested scrolling layout

--- androidApp/src/main/java/app/halfmouth/android/screen/HomeScreen.kt
+++ androidApp/src/main/java/app/halfmouth/android/screen/HomeScreen.kt
@@ -11,13 +11,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Text
@@ -39,9 +38,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import app.halfmouth.android.R
 import app.halfmouth.android.components.BottomBarMenu
-import app.halfmouth.theme.OnBackgroundDark
 import app.halfmouth.theme.OnSurfaceVariantDark
-import app.halfmouth.theme.OutlineDark
 import app.halfmouth.theme.SurfaceVariantDark
 import app.halfmouth.theme.YellowContainerLight
 
@@ -50,6 +47,7 @@ fun HomeScreen(navController: NavHostController) {
     BackHandler { }
 
     Scaffold(
+        modifier = Modifier.background(SurfaceVariantDark),
         scaffoldState = rememberScaffoldState(),
         bottomBar = {
             BottomBarMenu(navController = navController)
@@ -60,33 +58,26 @@ fun HomeScreen(navController: NavHostController) {
                 .padding(it)
                 .background(Color.Black),
         ) {
-
             Box(
                 modifier = Modifier.fillMaxSize()
             ) {
                 Canvas(
                     modifier = Modifier
                         .fillMaxSize()
-                        .height(80.dp)
-                        .padding(top = 80.dp)
+                        .padding(top = 50.dp)
 
                 ) {
                     val size = size
                     drawRoundRect(
                         SurfaceVariantDark,
-                        topLeft = Offset(0f, 0f),
+                        topLeft = Offset(0f, 100f),
                         size = size,
                         cornerRadius = CornerRadius(30.dp.toPx(), 30.dp.toPx()),
                     )
-
                 }
             }
         }
-
-        Column(
-            modifier = Modifier
-
-        ) {
+        Column {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -94,7 +85,6 @@ fun HomeScreen(navController: NavHostController) {
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center
             ) {
-
                 Image(
                     painter = painterResource(id = R.drawable.perfil_default),
                     contentDescription = "",
@@ -103,7 +93,6 @@ fun HomeScreen(navController: NavHostController) {
                         .clip(RoundedCornerShape(35))
                 )
             }
-
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -122,86 +111,80 @@ fun HomeScreen(navController: NavHostController) {
                     ),
                 )
             }
-
-
-            Row(
+            LazyColumn(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Start
-            ) {
-                Text(
-                    text = "Nossos Ingredientes:",
-                    style = TextStyle(
-                        color = OnSurfaceVariantDark,
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        fontFamily = FontFamily.SansSerif,
-                        textAlign = TextAlign.Center
-                    ),
-                )
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.lupulo),
-                    contentDescription = "",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 10.dp, end = 10.dp)
-                        .clip(RoundedCornerShape(16))
-                )
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Start
-            ) {
-                Text(
-                    text = "Cervejas produzidas:",
-                    style = TextStyle(
-                        color = OnSurfaceVariantDark,
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        fontFamily = FontFamily.SansSerif,
-                        textAlign = TextAlign.Center
-                    ),
-                )
-            }
-
-            LazyVerticalGrid(
-                modifier = Modifier
+                    .fillMaxSize()
                     .padding(bottom = 50.dp),
-                columns = GridCells.Fixed(2),
                 contentPadding = PaddingValues(16.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                items(8) {
-                    Card(
-                        modifier = Modifier
-                            .background(SurfaceVariantDark)
-                            .fillMaxWidth()
-                            .height(100.dp),
-                        shape = RoundedCornerShape(16.dp),
-                        elevation = 4.dp,
-                        backgroundColor = OnBackgroundDark
-                    ) {}
-
-                }
-
+                item { SubItemTitle() }
+                item { SubListIngredients() }
+                subListBeerProduction()
             }
-
         }
+    }
+}
+
+@Composable
+fun SubItemTitle() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Text(
+            modifier = Modifier.padding(bottom = 8.dp),
+            text = "Nossos Ingredientes:",
+            style = TextStyle(
+                color = OnSurfaceVariantDark,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Start
+            ),
+        )
+    }
+}
+
+@Composable
+fun SubListIngredients() {
+    LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        items(10) {
+            Image(
+                painter = painterResource(id = R.drawable.lupulo),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(RoundedCornerShape(16))
+            )
+        }
+    }
+}
+
+fun LazyListScope.subListBeerProduction() {
+    items(8) {
+        if (it == 0) {
+            Text(
+                modifier = Modifier.padding(top = 8.dp),
+                text = "Nossas Cervejas:",
+                style = TextStyle(
+                    color = OnSurfaceVariantDark,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.SansSerif,
+                    textAlign = TextAlign.Center
+                ),
+            )
+        }
+        Image(
+            painter = painterResource(id = R.drawable.imagebeerbackground),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp, bottom = 8.dp)
+                .clip(RoundedCornerShape(16))
+        )
     }
 }
