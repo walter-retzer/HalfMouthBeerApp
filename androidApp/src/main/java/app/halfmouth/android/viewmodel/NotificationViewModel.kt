46bef4f33148de1d167704574aacdfb51c3a3e06
WDRETZER <walter_retzer@yahoo.com.br>
2023-12-07 18:24:35 -0300

    Ajuste do código para receber a lista de Notificações do Firebase RealTimeDatabase.

--- androidApp/src/main/java/app/halfmouth/android/viewmodel/NotificationViewModel.kt
+++ androidApp/src/main/java/app/halfmouth/android/viewmodel/NotificationViewModel.kt
@@ -1,9 +1,7 @@
 package app.halfmouth.android.viewmodel
 
 import android.util.Log
-import android.widget.Toast
 import app.halfmouth.android.data.firebase.RealTimeItemFirebase
-import app.halfmouth.android.main.AndroidApp
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -11,53 +9,42 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.getValue
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class NotificationViewModel: ViewModel(){
+class NotificationViewModel : ViewModel() {
 
-    private val _message = MutableStateFlow("")
-    val message = _message.asStateFlow()
+    private var _listNotifications = MutableStateFlow(emptyList<RealTimeItemFirebase>())
+    val listNotifications: StateFlow<List<RealTimeItemFirebase>> = _listNotifications
 
-    init{
+    init {
         getInfoFirebaseRealTimeDatabase()
     }
 
     private fun getInfoFirebaseRealTimeDatabase() {
-        val context = AndroidApp.applicationContext
         val firebaseDB = FirebaseDatabase.getInstance()
         val ref = firebaseDB.getReference("notifications")
-        var itemUpdate: RealTimeItemFirebase? = null
 
         ref.addValueEventListener(object : ValueEventListener {
-
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
                 try {
                     val matches = dataSnapshot.getValue<HashMap<String, RealTimeItemFirebase>>()
                     matches?.let {
                         val item = it.values
-
-                        item.map { notifications ->
-                            itemUpdate = notifications
+                        item.forEach { notifications ->
+                            val tempList = _listNotifications.value.toMutableList()
+                            tempList.add(notifications)
+                            _listNotifications.value = tempList
                         }
                     }
-                    _message.value = itemUpdate?.message.toString()
-
-                    Toast.makeText(context, "Firebase - message: ${itemUpdate?.message}", Toast.LENGTH_LONG).show()
-                    Toast.makeText(context, "Firebase - data: ${itemUpdate?.date}", Toast.LENGTH_LONG).show()
-
                 } catch (e: Exception) {
-                    Toast.makeText(context, "Erro $e", Toast.LENGTH_LONG).show()
                     Log.d("Error", "Error $e")
                 }
-
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.i("firebase", "Error to read Firebase!", error.toException())
             }
         })
-
     }
-
 }
