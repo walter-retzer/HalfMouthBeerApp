7db12b439177ecd3bb20770f9239661892476444
WDRETZER <walter_retzer@yahoo.com.br>
2023-12-07 19:16:50 -0300

    Inclusão de variavel na data class RealTimeItemFirebase

--- androidApp/src/main/java/app/halfmouth/android/data/firebase/RealTimeItemFirebase.kt
+++ androidApp/src/main/java/app/halfmouth/android/data/firebase/RealTimeItemFirebase.kt
@@ -2,5 +2,6 @@ package app.halfmouth.android.data.firebase
 
 data class RealTimeItemFirebase(
     var data: String? = "",
-    var message: String? = ""
+    var message: String? = "",
+    var index: Int = 0
 )
