4652d39c8ce07047d9ca706a253f5f31ad7d61c1
WDRETZER <walter_retzer@yahoo.com.br>
2023-12-08 16:04:42 -0300

    Ajuste da fun de deletar conta do usuario

--- androidApp/src/main/java/app/halfmouth/android/viewmodel/ProfileViewModel.kt
+++ androidApp/src/main/java/app/halfmouth/android/viewmodel/ProfileViewModel.kt
@@ -15,7 +15,6 @@ import app.halfmouth.android.utils.Constants.Companion.USER_EMAIL
 import app.halfmouth.android.utils.Constants.Companion.USER_GOOGLE_SIGNIN
 import app.halfmouth.android.utils.Constants.Companion.USER_IMAGE
 import app.halfmouth.android.utils.Constants.Companion.USER_NAME
-import app.halfmouth.android.utils.Constants.Companion.USER_NULL
 import app.halfmouth.android.utils.Constants.Companion.USER_UID
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.firebase.auth.FirebaseAuth
@@ -28,7 +27,6 @@ import kotlinx.coroutines.launch
 class ProfileViewModel : ViewModel() {
     private val pref = SecurePreferencesApp()
     private val signInGoogle = pref.get(USER_GOOGLE_SIGNIN) ?: false
-    private val signInDefault = pref.get(USER_DEFAULT_SIGNIN) ?: false
 
     var isClicked by mutableStateOf(false)
 
@@ -38,35 +36,12 @@ class ProfileViewModel : ViewModel() {
     private val _signInState = MutableStateFlow(SignInState())
     val signInState = _signInState.asStateFlow()
 
-    private val _email = MutableStateFlow(USER_NULL)
-    val email = _email.asStateFlow()
-
-    private val _phone = MutableStateFlow(USER_NULL)
-    val phone = _phone.asStateFlow()
-
-    private val _name = MutableStateFlow(USER_NULL)
-    val name = _name.asStateFlow()
-
-    private val _image = MutableStateFlow(USER_NULL)
-    val image = _image.asStateFlow()
-
     private val googleAuthUiClient by lazy {
         GoogleAuthUiClient(oneTapClient = Identity.getSignInClient(AndroidApp.applicationContext))
     }
 
     init {
-        if (signInGoogle) {
-            googleAuthUiClient.getSignedInUser()
-            getPref()
-        }
-        if (signInDefault) getPref()
-    }
-
-    private fun getPref() {
-        _email.value = pref.get(USER_EMAIL) ?: USER_NULL
-        _phone.value = pref.get(USER_CELLPHONE) ?: USER_NULL
-        _name.value = pref.get(USER_NAME) ?: USER_NULL
-        _image.value = pref.get(USER_IMAGE) ?: USER_NULL
+        if (signInGoogle) googleAuthUiClient.getSignedInUser()
     }
 
     fun deleteAccount() {
