31aa1a81df764deb9520160599ce9d12df88139d
WDRETZER <walter_retzer@yahoo.com.br>
2023-12-08 16:08:52 -0300

    Adicionado fun formattedAsPhone() para exibir uma string no formato da mascara do celular.

--- androidApp/src/main/java/app/halfmouth/android/utils/Extensions.kt
+++ androidApp/src/main/java/app/halfmouth/android/utils/Extensions.kt
@@ -22,3 +22,22 @@ fun String.formattedDate(): String {
     }
     return string
 }
+
+fun String.formattedAsPhone(): String {
+    return when (length) {
+        11 -> "(" + substring(0, 2) + ") " +
+                substring(2, 3) + " " +
+                substring(3, 7) + "-" +
+                substring(7, length)
+        10 -> "(" + substring(0, 2) + ") " +
+                substring(2, 6) + "-" +
+                substring(6, length)
+
+        14 -> substring(0, 3) + " (" + substring(3, 5) + ") " +
+                substring(5, 7) +
+                substring(7, 10) + "-" +
+                substring(10, length)
+
+        else -> this
+    }
+}
